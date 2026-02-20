79c3d9f236d276f544c5a5ead49d8b79c01f8769
rahafalharthi1111-png <rahaf.alharthi1111@gmail.com>
2026-02-20 13:49:30 +0000

    add

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Module for string concatenation.
+
+Functions:
+    concat(str1: str, str2: str) -> str: Concatenates two strings.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenated string.
+    """
+    return str1 + str2
