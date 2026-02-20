1704852fdcf5029cf88636329315fe5a4f5d01b5
rahafalharthi1111-png <rahaf.alharthi1111@gmail.com>
2026-02-20 14:01:06 +0000

    add

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module that defines and annotates basic variables.
+
+Attributes:
+    a (int): An integer with a value of 1.
+    pi (float): A float with a value of 3.14.
+    i_understand_annotations (bool): A boolean with a value of True.
+    school (str): A string with a value of "Holberton".
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
