978e141d7c8be3def949eaa35c2a75fe006ad6a4
rahafalharthi1111-png <rahaf.alharthi1111@gmail.com>
2026-02-20 13:56:54 +0000

    add

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module for converting a floating point number to a string.
+
+Functions:
+    to_str(n: float) -> str: Converts a float to its string representation.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Converts a floating point number to a string.
+
+    Args:
+        n (float): The number to convert.
+
+    Returns:
+        str: The string representation of the number.
+    """
+    return str(n)
