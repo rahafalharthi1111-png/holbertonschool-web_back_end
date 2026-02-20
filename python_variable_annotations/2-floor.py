05143faf80dfa7410be591b099d7c10172c8ff8b
rahafalharthi1111-png <rahaf.alharthi1111@gmail.com>
2026-02-20 13:53:03 +0000

    add

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module for computing the floor of a floating point number.
+
+Functions:
+    floor(n: float) -> int: Returns the floor of a float.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a floating point number.
+
+    Args:
+        n (float): The number to floor.
+
+    Returns:
+        int: The largest integer less than or equal to n.
+    """
+    return int(n)
