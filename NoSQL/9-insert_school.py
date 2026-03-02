9e161815c3a6917bd7416049d0e5df8252b3d168
root <root@ip-10-42-15-125.ec2.internal>
2026-03-02 21:15:59 +0000

    add

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module for inserting documents in MongoDB
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document in a collection based on kwargs
+    Returns the new _id
+    """
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
