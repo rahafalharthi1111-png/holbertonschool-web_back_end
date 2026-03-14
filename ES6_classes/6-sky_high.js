08e1f1f04c3124cfdb8c2cc60b06aa285d3b58b8
root <root@ip-10-42-160-213.ec2.internal>
2026-03-14 19:13:14 +0000

    add

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,19 @@
+import Building from './5-building.js';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft); // on passe sqft au constructeur parent
+    if (typeof floors !== 'number') {
+      throw new TypeError('floors must be a number');
+    }
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
